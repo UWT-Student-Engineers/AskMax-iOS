637e1b5c7493c09911aa53eae1b01b3906635ca3
Andrew Gregory <Dhaerak@D-10-16-28-183.dhcp4.washington.edu>
2016-02-04 14:16:27 -0800

    test commit

--- AskMax-iOS/ViewController.swift
+++ AskMax-iOS/ViewController.swift
@@ -18,6 +18,12 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
+        
+        
+        
+        
+        // Testing comment - Andrew 
     }
 
 
